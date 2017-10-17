79974739a8f82192af5af42b4c25855b44f04ae8
Griffin P Longtin and Grant Rotzien <griffin@>
2017-10-17 16:43:55 -0700

    this adds all the jquery button

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,76 @@ $(function() {
     $('.JavaScript-card-back').slideToggle();
     $('.JavaScript-card-front').slideToggle();
   });
-
-
-
-
-
-
-
-
-
+  $('.Operators').click(function(){
+    $('.Operators-card-back').slideToggle();
+    $('.Operators-card-front').slideToggle();
+  });
+  $('.Variables').click(function(){
+    $('.Variables-card-back').slideToggle();
+    $('.Variables-card-front').slideToggle();
+  });
+  $('.Variable-name-convention').click(function(){
+    $('.Variable-name-convention-card-back').slideToggle();
+    $('.Variable-name-convention-card-front').slideToggle();
+  });
+  $('.Function').click(function(){
+    $('.Function-card-back').slideToggle();
+    $('.Function-card-front').slideToggle();
+  });
+  $('.Methods').click(function(){
+    $('.Methods-card-back').slideToggle();
+    $('.Methods-card-front').slideToggle();
+  });
+  $('.Arguments').click(function(){
+    $('.Arguments-card-back').slideToggle();
+    $('.Arguments-card-front').slideToggle();
+  });
+  $('.Parameters').click(function(){
+    $('.Parameters-card-back').slideToggle();
+    $('.Parameters-card-front').slideToggle();
+  });
+  $('.Return').click(function(){
+    $('.Return-card-back').slideToggle();
+    $('.Return-card-front').slideToggle();
+  });
+  $('.Chaining-methods').click(function(){
+    $('.Chaining-methods-card-back').slideToggle();
+    $('.Chaining-methods-card-front').slideToggle();
+  });
+  $('.Strings').click(function(){
+    $('.Strings-card-back').slideToggle();
+    $('.Strings-card-front').slideToggle();
+  });
+  $('.Booleans').click(function(){
+    $('.Booleans-card-back').slideToggle();
+    $('.Booleans-card-front').slideToggle();
+  });
+  $('.Undefined').click(function(){
+    $('.Undefined-card-back').slideToggle();
+    $('.Undefined-card-front').slideToggle();
+  });
+  $('.NaN').click(function(){
+    $('.NaN-card-back').slideToggle();
+    $('.NaN-card-front').slideToggle();
+  });
+  $('.Escape').click(function(){
+    $('.Escape-card-back').slideToggle();
+    $('.Escape-card-front').slideToggle();
+  });
+  $('.Alert').click(function(){
+    $('.Alert-card-back').slideToggle();
+    $('.Alert-card-front').slideToggle();
+  });
+  $('.Comments').click(function(){
+    $('.Comments-card-back').slideToggle();
+    $('.Comments-card-front').slideToggle();
+  });
+  $('.jQuery').click(function(){
+    $('.jQuery-card-back').slideToggle();
+    $('.jQuery-card-front').slideToggle();
+  });
+  $('.Attributes').click(function(){
+    $('.Attributes-card-back').slideToggle();
+    $('.Attributes-card-front').slideToggle();
+  });
 });
